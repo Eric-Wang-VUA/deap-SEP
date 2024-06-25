becd085e319c22cd42f1618967d8ee1785806c76
Dan Zou <d.zou2@student.vu.nl>
2024-06-26 01:18:32 +0200

    update manual tool

--- tool-DZ.py
+++ tool-DZ.py
@@ -10,10 +10,11 @@ print("Test for trap function!")
 test_trap()
 
 def test_inv_trap():
-    result = binary.inv_trap([1, 1, 1])
-    binary.print_coverage_inv_trap()
     
     result = binary.inv_trap([0, 0, 0])
+    binary.print_coverage_inv_trap()
+
+    result = binary.inv_trap([1, 1, 1])
     binary.print_coverage_inv_trap()
 
 
